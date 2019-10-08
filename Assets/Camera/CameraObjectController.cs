0984c33c9d0cfcce2865848174d0153382b1cdfd
csugianto <csugianto@student.unimelb.edu.au>
2019-10-09 00:49:57 +1100

    resize basic floor and walls, add temporary cameraObject with its movement script, and place the camera in a good viewing place

---
+++ Assets/Camera/CameraObjectController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraObjectController : MonoBehaviour
+{
+    /**************** attributes ****************/
+
+    public float mouseSensitivity = 0.5f;           // Mouse rotation sensitivity.
+    [Range(0.0f, 35.0f)]
+    public float speed = 5f;                        // Speed of the camera object.
+    
+    private float rotationY;                        // Camera Rotation in Y Axis
+    private float rotationX;                        // Camera Rotation in X Axis
+    private bool isRotating;                        // Determines if Camera is Rotating
+    /********************************************/
+
+    /**
+     * Move using WASD
+     * Hold left click to rotate camera
+     * Q/E to raise/lower Y plane
+     */
+
+    void Start()
+    {
+        // To prevent rotation to be set to 0 at first click of mouse
+        this.rotationY = -transform.localEulerAngles.x;
+        this.rotationX = transform.localEulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // Handles Rotation of camera
+		// Assing isRotating when left click is being pressed
+		if (Input.GetMouseButtonDown(0))
+			isRotating = true;
+       
+		// Assing isRotating to false when left click isnt being pressed
+		if (Input.GetMouseButtonUp(0))
+			isRotating = false;
+
+		if (isRotating)
+        { 
+		// Handles Rotation of camera
+            rotationX += Input.GetAxis("Mouse X") * mouseSensitivity;
+            rotationY += Input.GetAxis("Mouse Y") * mouseSensitivity;
+            rotationY = Mathf.Clamp(rotationY, -90, 90);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0.0f);
+        }
+
+        // Handles Keyboard input
+        Vector3 position = new Vector3();
+        if (Input.GetKey(KeyCode.D))
+            position.x += 1.0f;
+        if (Input.GetKey(KeyCode.A))
+            position.x -= 1.0f;
+        if (Input.GetKey(KeyCode.W))
+            position.z += 1.0f;
+        if (Input.GetKey(KeyCode.S))
+            position.z -= 1.0f;
+        if (Input.GetKey(KeyCode.E))
+            position.y -= 1.0f;
+        if (Input.GetKey(KeyCode.Q))
+            position.y += 1.0f;
+        
+		position *= speed * Time.deltaTime;
+        transform.Translate(position);
+    }
+
+}
