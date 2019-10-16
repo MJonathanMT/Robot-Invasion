5c436cc8e5ac59601d4647c5d8c4cc12bb8341db
csugianto <csugianto@student.unimelb.edu.au>
2019-10-17 00:06:09 +1100

    merge with main-menu and .gitignore fix

---
+++ Assets/MainMenuAssets/Scripts/CharacterRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotation : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        rotateObject();
+        
+    }
+
+    public void rotateObject() {
+        transform.Rotate (new Vector3(0, Time.deltaTime * speed, 0));
+    }
+}
---
+++ Assets/MainMenuAssets/Scripts/CharacterSelection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public GameObject character;
+    public int width = 1920;
+    public int height = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y=0; y<height; ++y)
+       {
+           for (int x=0; x<width; ++x)
+           {
+               Instantiate(character, new Vector3(x,y,0), Quaternion.identity);
+           }
+       }       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
