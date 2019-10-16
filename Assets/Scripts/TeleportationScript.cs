41766da1c231b90ce5445bdf77d03fe9b45bd4c0
csugianto <csugianto@student.unimelb.edu.au>
2019-10-16 18:10:46 +1100

    add player teleportation when player collides with portals

---
+++ Assets/Scripts/TeleportationScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject portal2;
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            player.transform.position = portal2.transform.position + new Vector3(3.0f, 0.0f, 0.0f);
+        }
+    }
+}
