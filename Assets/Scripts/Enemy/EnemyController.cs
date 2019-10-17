df9855ad0a211c8307e263d0aee4f88e1b0408e5
csugianto <csugianto@student.unimelb.edu.au>
2019-10-18 10:11:43 +1100

    Revert "Revert "add enemy shooting""
    This reverts commit c4f93624043818ffdc7f3d217a71f879db322f42.

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -9,6 +9,12 @@ public class EnemyController : MonoBehaviour
     public GameObject destroyExplosionPrefab;
     // Audio Source 
     private AudioSource explosionAudioSrc;
+
+    public BulletController enemyBulletPrefab;
+    private float nextTimeToFire = 1f;
+    private float timer;
+    private float fireRate = 5f;
+    public int attackRange;
  
     void Start ()
 
@@ -21,6 +27,21 @@ public class EnemyController : MonoBehaviour
     void Update ()
     {
         _nav.SetDestination(_player.position);
+
+        // shooting player
+        if ((Vector3.Distance(_player.transform.position, this.transform.position) < attackRange) && (Time.time >= nextTimeToFire)) {
+            timer = 0.0f;
+            nextTimeToFire = Time.time + 1f / fireRate;
+            shootBullet();
+        }
+    }
+
+    // shooting bullets
+    public void shootBullet()
+    {
+        BulletController p = Instantiate<BulletController>(enemyBulletPrefab);
+        p.transform.position = this.transform.position + new Vector3( 1f,1f,1f);
+        p.velocity = (_player.transform.position - this.transform.position).normalized * 10.0f;
     }
 
     // // This should be hooked up to the health manager on this object
