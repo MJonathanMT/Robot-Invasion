8ca4dd87838dc62108635d207f9fff40fb9e22e9
Kirentheren Ravindra Sanmugam <ksanmugam@student.unimelb.edu.au>
2019-10-17 16:23:33 +1100

    Enemy bullet

--- Assets/Scripts/Bullet/BulletController.cs
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    public Vector3 velocity = new Vector3(0,0,25);
+    public Vector3 velocity = new Vector3(0,1f,0);
+    
 
     // Update is called once per frame
     void Update () {
+        velocity.y = 0;
         this.transform.Translate(velocity * Time.deltaTime);
 	}
 
     void OnCollisionEnter (Collision co){
         velocity = new Vector3(0,0,0);
-        Destroy (this.gameObject); 
+       Destroy (this.gameObject);
+
+       /* if (co.gameObject.tag == "Player")
+        {
+            // Damage object with relevant tag
+            //HealthManager healthManager = col.gameObject.GetComponent<HealthManager>();
+            //healthManager.ApplyDamage(damageAmount);
+
+            // Destroy self
+            Destroy(this.gameObject);
+        } */
     }
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,17 +4,38 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     private NavMeshAgent _nav;
-    private Transform _player;
- 
-    void Start ()
+    private PlayerController player;
+    public BulletController enemyBulletPrefab;
+    private float nextTimeToFire = 1f;
+    private float timer;
+    private float fireRate = 5f;
+    public int attackRange;
 
+    void Start ()
     {
         _nav = GetComponent<NavMeshAgent>();
-        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        // Get player reference if none attached already
+        if (this.player == null)
+        {
+            this.player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        }
     }
      
     void Update ()
     {
-        _nav.SetDestination(_player.position);
+        _nav.SetDestination(player.transform.position);
+
+        if ((Vector3.Distance(player.transform.position, this.transform.position) < attackRange) && (Time.time >= nextTimeToFire)) {
+            timer = 0.0f;
+            nextTimeToFire = Time.time + 1f / fireRate;
+            shootBullet();
+        }
+    }
+
+    public void shootBullet()
+    {
+        BulletController p = Instantiate<BulletController>(enemyBulletPrefab);
+        p.transform.position = this.transform.position + new Vector3( 1f,1f,1f);
+        p.velocity = (player.transform.position - this.transform.position).normalized * 10.0f;
     }
 }
