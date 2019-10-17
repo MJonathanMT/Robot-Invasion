cf08c07a6286e07122fabd133819d6b03718d9c2
csugianto <csugianto@student.unimelb.edu.au>
2019-10-17 12:52:33 +1100

    add enemy AI pathing

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,41 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.AI;
+ 
 public class EnemyController : MonoBehaviour
 {
-    public Transform player;
-    private Rigidbody rb;
-    private Vector2 movement;
+    private NavMeshAgent _nav;
+    private Transform _player;
+ 
+    void Start ()
 
-    // Start is called before the first frame update
-    void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        _nav = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
     }
-
-    // Update is called once per frame
-    void Update()
+     
+    void Update ()
     {
-        // get position of target object
-        Vector3 targetPosition = player.transform.position;
-
-        // gives us vector to direction of target
-        Vector3 inverseVect = transform.InverseTransformPoint(targetPosition);
-
-        // calculate angle by which you have to rotate
-        // Note -: This angle is calculated every Frame of FixedUpdate
-        float rotationAngle = Mathf.Atan2(inverseVect.x, inverseVect.z) * Mathf.Rad2Deg;
-
-        // Now calculate  rotationVelocity to be applied every frame
-        Vector3 rotationVelocity = (Vector3.up * rotationAngle) * 10.0f * Time.deltaTime;
-
-        // Calaculate his delta velocity   i.e required - current 
-        Vector3 deltavel = (rotationVelocity - rb.angularVelocity);
-
-        // Apply the force to rotate
-        rb.AddTorque(deltavel, ForceMode.Impulse);
-
-        transform.position += transform.forward * 2.0f * Time.deltaTime;
+        _nav.SetDestination(_player.position);
     }
 }
