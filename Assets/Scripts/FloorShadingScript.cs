ba4d603692b76e7d5930c8a6217d4001d6a72198
Unknown <rohan.australia@gmail.com>
2019-10-18 10:59:54 +1100

    added sun and a spot light

---
+++ Assets/Scripts/FloorShadingScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorShadingScript : MonoBehaviour
+{
+    public Shader shader;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+        GetComponent<Renderer>().material.shader = shader;
+    }
+
+    // Called each frame
+    void Update()
+    {
+        // Get renderer component (in order to pass params to shader)
+        MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
+    }
+
+}
---
+++ Assets/Scripts/TurnOnSpotLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnSpotLight : MonoBehaviour
+{
+    public GameObject sun;
+    public Light spotlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+         Debug.Log(sun.transform.localEulerAngles.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (sun.transform.localEulerAngles.z < 300 && sun.transform.localEulerAngles.z > 80)
+        {
+           
+            spotlight.enabled = true;
+        }
+
+        else {
+            spotlight.enabled = false;
+        }
+    }
+}
