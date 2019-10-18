12eeaaa0e0a3b52d70f9a033316d4192812c0b16
csugianto <csugianto@student.unimelb.edu.au>
2019-10-18 13:28:28 +1100

    add EndGame scene

---
+++ Assets/Scripts/SceneControllers/EndGameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndGameController : MonoBehaviour {
+    
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OpenInGame()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+}
